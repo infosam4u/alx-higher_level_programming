368af98ada4a76c235f2e5ac0f25f762537dcc2a
infosam4u <angelsvoiceton@gmail.com>
2022-12-29 14:49:00 +0000

    otilo

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"First 3 letters: {word[0:3]}")
 # To printlast two words
-print(f"Last 2 letters: {word[7:9]}")
+print(f"Last 2 letters: {word[-2:]}")
 # to print the middle words
-print(f"Middle word: {word[1:8]}")
+print(f"Middle word: {word[1:-1]}")

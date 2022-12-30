e2e1b1ba0b3ac1b1461c6317c730d83ce7565501
infosam4u <angelsvoiceton@gmail.com>
2022-12-30 22:13:24 +0000

    egde

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"First 3 letters: {word[0:3]}")
-# To printlast two words
-print(f"Last 2 letters: {word[-2:]}")
-# to print the middle words
-print(f"Middle word: {word[1:-1]}")
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))

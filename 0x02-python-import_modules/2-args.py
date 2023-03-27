ce41eb14e103d3243b7d6c9545c5ca0851b7c38c
infosam4u <angelsvoiceton@gmail.com>
2023-03-27 15:05:35 +0100

    task 0x02

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of argument and its list."""
+    import sys
+
+    total = len(sys.argv) - 1
+    if total == 0:
+        print("0 arguments.")
+    elif total == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(total))
+    for i in range(total):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,21 @@
+#**This is a Project on Import and Modules.**
+
+####Task 0: 
+*Imports a simple function from a simple file*
+
+####Task 1: 
+*is 'My first toolbox'*
+
+####Task 2: 
+*How to make a script dynamic*
+
+####Task 3: 
+*Infinit addition*
+
+####Task 4: 
+*Who are you?*
+
+####Task 5: 
+*Everything can be imported*
+
+And four (4) more advanced Task
